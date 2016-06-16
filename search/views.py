f0c592379b64bf38c9d5127795be7092e247a44d
Kohli <sukohli@deloitte.com>
2016-06-16 17:21:36 +0530

    Removed Unused code and comments

--- search/views.py
+++ search/views.py
@@ -2,18 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse,Http404
 import requests
 import code
-import pdb
-
-# Create your views here.
-# def index(request):
-#     latest_question_list = ['suraj','kohli','delhi','noida','india']
-#     output = '<br> '.join([q for q in latest_question_list])
-#     return HttpResponse(output)
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'search/index.html', context) 
 
 def index(request):
 	return render(request, 'search/index.html')  
@@ -61,7 +49,6 @@ def results(request):
 		message = 'Empty Form'
 		return render( request, 'search/index.html')
 
-	# url = 'https://api.coursera.org/api/courses.v1?q=search&query=' + request.GET['query']
 	frontQuery = 'https://api.coursera.org/api/courses.v1?q=search&query='
 	EndQuery = '&includes=instructorIds,partnerIds,partnerLogo&fields=instructorIds,partnerIds,partnerLogo'
 	url =  frontQuery + request.GET['query'] + EndQuery
@@ -69,15 +56,6 @@ def results(request):
 		response_dict = requests.get(url)
 	except:
 		return HttpResponse('<h1>No Internet Connection</h1>')		
-	# r = response_dict.content
-	# r = json.loads(response_dict.content)
 	courses=response_dict.json()
-	# courses=courses["elements"]
-	# courses = courses.json()
-	# updated_instructors_courses = fetchInstructors(courses)
-	# updated_partners_courses = fetchPartners(updated_instructors_courses)
-	# code.interact(local=locals())	
-	# context = { 'courses': updated_partners_courses }
 	context = { 'courses': courses }
 	return render(request, 'search/results.html' , context)
-	# return HttpResponse(response_dict)
