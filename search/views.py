13ed4e068f35cf1a092f5d67be5aba9df382bffc
Kohli <sukohli@deloitte.com>
2016-06-18 20:00:06 +0530

    Modified .gitignore

--- search/views.py
+++ search/views.py
@@ -2,45 +2,44 @@ from django.shortcuts import render
 from django.http import HttpResponse,Http404
 import requests
 import code
-
 def index(request):
 	return render(request, 'search/index.html')  
 
 
-def fetchInstructors(courses):
-	startUrl = 'https://api.coursera.org/api/instructors.v1?ids='
-	endUrl = '&includes=firstName,lastName&fields=firstName,lastName'
-	for course in courses:
-		instructorIds = course['instructorIds']
-		comma_separated_instructorIds = ",".join(instructorIds)
-		url = startUrl + comma_separated_instructorIds + endUrl
-		instructors = requests.get(url)				
-		instructors = instructors.json()
-		instructors = instructors["elements"]
-		list_of_instructorNames = list()
-		for instructor in instructors:
-			name = instructor['firstName'] + ' ' + instructor['lastName']
-			list_of_instructorNames.append(name)
-		course['instructorNames'] = list_of_instructorNames
-		# code.interact(local=locals())
-	return courses
+# def fetchInstructors(courses):
+# 	startUrl = 'https://api.coursera.org/api/instructors.v1?ids='
+# 	endUrl = '&includes=firstName,lastName&fields=firstName,lastName'
+# 	for course in courses:
+# 		instructorIds = course['instructorIds']
+# 		comma_separated_instructorIds = ",".join(instructorIds)
+# 		url = startUrl + comma_separated_instructorIds + endUrl
+# 		instructors = requests.get(url)				
+# 		instructors = instructors.json()
+# 		instructors = instructors["elements"]
+# 		list_of_instructorNames = list()
+# 		for instructor in instructors:
+# 			name = instructor['firstName'] + ' ' + instructor['lastName']
+# 			list_of_instructorNames.append(name)
+# 		course['instructorNames'] = list_of_instructorNames
+# 		# code.interact(local=locals())
+# 	return courses
 
-def fetchPartners(courses):
-	startUrl = 'https://api.coursera.org/api/partners.v1?ids='
-	for course in courses:
-		partnerIds = course['partnerIds']
-		comma_separated_partnerIds = ",".join(partnerIds)		
-		url = startUrl + comma_separated_partnerIds
-		partners = requests.get(url)
-		partners = partners.json()
-		partners = partners["elements"]
-		list_of_partnerNames = list()		
-		for partner in partners:
-			name = partner['name']
-			list_of_partnerNames.append(name)
-		course['partnerNames'] = list_of_partnerNames
-		# code.interact(local=locals())
-	return courses	
+# def fetchPartners(courses):
+# 	startUrl = 'https://api.coursera.org/api/partners.v1?ids='
+# 	for course in courses:
+# 		partnerIds = course['partnerIds']
+# 		comma_separated_partnerIds = ",".join(partnerIds)		
+# 		url = startUrl + comma_separated_partnerIds
+# 		partners = requests.get(url)
+# 		partners = partners.json()
+# 		partners = partners["elements"]
+# 		list_of_partnerNames = list()		
+# 		for partner in partners:
+# 			name = partner['name']
+# 			list_of_partnerNames.append(name)
+# 		course['partnerNames'] = list_of_partnerNames
+# 		# code.interact(local=locals())
+# 	return courses	
 
 def results(request):
 	if 'query' in request.GET:
